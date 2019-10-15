6640decd041f38b7c98178c82b589e7bc0c37d8d
Henrique Morbin <henrique.morbin@ifood.com.br>
2019-10-15 20:48:25 -0300

    Adding MD5 hash generator and removing UITest for a while.

---
+++ MarvelTests/MD5Tests.swift
@@ -0,0 +1,13 @@
+import XCTest
+@testable import Marvel
+
+final class MD5Tests: XCTestCase {
+
+    func testExample() {
+        let hashGenerated =  "dummy content".md5
+        let hashExpected = "90c55a38064627dca337dfa5fc5be120"
+
+        XCTAssertEqual(hashGenerated, hashExpected)
+    }
+
+}
