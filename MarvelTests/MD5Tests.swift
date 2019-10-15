d6f3c14695fd0883d4e5b4d8659c9c360717d8d3
Henrique Morbin <henrique.morbin@ifood.com.br>
2019-10-15 20:48:49 -0300

    Renaming test.

--- MarvelTests/MD5Tests.swift
+++ MarvelTests/MD5Tests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 final class MD5Tests: XCTestCase {
 
-    func testExample() {
+    func testHash() {
         let hashGenerated =  "dummy content".md5
         let hashExpected = "90c55a38064627dca337dfa5fc5be120"
 
