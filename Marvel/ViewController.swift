d7392c8dcf2c81c31e685843950d87d3c96f5545
Henrique Morbin <henrique.morbin@ifood.com.br>
2019-10-15 20:09:54 -0300

    Removing Storyboard.

--- Marvel/ViewController.swift
+++ Marvel/ViewController.swift
@@ -1,20 +1,11 @@
-//
-//  ViewController.swift
-//  Marvel
-//
-//  Created by Henrique Morbin on 15/10/19.
-//  Copyright © 2019 Henrique Morbin. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .red
     }
-
-
 }
 
