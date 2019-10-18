b7b1fb126c7e8e787922d2799de73765dd0fc363
Henrique Morbin <henrique.morbin@ifood.com.br>
2019-10-18 00:35:56 -0300

    Adding reference

--- Marvel/Source/Extensions/String+Localizable.swift
+++ Marvel/Source/Extensions/String+Localizable.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+// https://medium.com/@marcosantadev/app-localization-tips-with-swift-4e9b2d9672c9
 extension String {
     func localized(bundle: Bundle = .main, tableName: String = "Localizable") -> String {
         return NSLocalizedString(self, tableName: tableName, value: "**\(self)**", comment: "")
